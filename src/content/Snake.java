a0607a8dc3f9731009b2d6b7c01ade2cb3456298
MateuszSz <mateuszszymczak15@gmail.com>
2016-03-21 00:15:12 +0100

    Collision first step
    Created collision check using mask table

--- src/content/Snake.java
+++ src/content/Snake.java
@@ -23,20 +23,40 @@ public class Snake {
 
     /*  changes the coordinates of snake's head (and convert old head to new body element)  */
     //change to boolean later(for collision)
-    public void move(){
-        body.add(head);                 //save head as a body now
+    public void move(int[][] mask){
+       // body.add(head);                 //save head as a body now
         switch (lastKey){
             case W:
-                head.translate(0,-1);   //x stays the same, y one upper
+                if(mask[this.head.x][this.head.y-1] == 0){        //check if              
+                    body.add(head);
+                    head.translate(0,-1);   //x stays the same, y one upper
+                }
+                else
+                    System.out.println("boom");
                 break;
             case S:
-                head.translate(0,1);    //x stays the same, y one down
+                if(mask[this.head.x][this.head.y+1] == 0){
+                    body.add(head);
+                    head.translate(0,1);    //x stays the same, y one down
+                }
+                else
+                    System.out.println("boom");
                 break;
             case A:
-                head.translate(-1,0);   //x one left, y stays the same
+                if(mask[this.head.x-1][this.head.y] == 0){
+                    body.add(head);
+                    head.translate(-1,0);   //x one left, y stays the same              
+                }
+                else
+                    System.out.println("boom");
                 break;
             case D:
-                head.translate(1,0);    //x one right, y stays the same
+                if(mask[this.head.x+1][this.head.y] == 0){
+                    body.add(head);
+                    head.translate(1,0);    //x one right, y stays the same
+                }
+                else
+                    System.out.println("boom");
                 break;
             default:                    //no other key is allowed currently(we can add something in here later)
                 break;
